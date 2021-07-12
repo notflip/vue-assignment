58006ca1ebbb1fef55d71834836318fad5a6f130
Miguel Stevens <miguel@notflip.be>
2021-07-12 16:58:22 +0200

    Load repositories and display on mount test

---
+++ src/components/RepositoryList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <ul>
+      <li
+        v-for="repo in repositories"
+        :key="repo.id"
+        data-testid="repository"
+    >{{ repo.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapState } from 'vuex';
+
+export default {
+  name: 'RepositoryList',
+  mounted() {
+    this.loadRepositories();
+  },
+  methods: mapActions({
+    loadRepositories: 'repositories/load',
+  }),
+  computed: mapState({
+    repositories: (state) => state.repositories.repositories,
+  }),
+};
+</script>
---
+++ tests/unit/RepositoryList.spec.js
@@ -0,0 +1,55 @@
+import Vuex from 'vuex';
+import { createLocalVue, shallowMount } from '@vue/test-utils';
+import RepositoryList from '../../src/components/RepositoryList.vue';
+
+describe('RepositoryList', () => {
+  const localVue = createLocalVue();
+  localVue.use(Vuex);
+
+  // Test load the repositories on mounting
+  it('loads repositories on mounting', () => {
+    const repositoryModule = {
+      namespaced: true,
+      actions: {
+        load: jest.fn().mockName('load'),
+      },
+    };
+
+    const store = new Vuex.Store({
+      modules: {
+        repositories: repositoryModule,
+      },
+    });
+
+    shallowMount(RepositoryList, { localVue, store });
+
+    expect(repositoryModule.actions.load).toHaveBeenCalled();
+  });
+
+  // Test display the repositories after loading
+  it('displays the repositories', () => {
+    const repositoryModule = {
+      namespaced: true,
+      state: {
+        repositories: [
+          { id: 1, name: 'repo 1' },
+          { id: 2, name: 'repo 2' },
+        ],
+      },
+      actions: {
+        load: jest.fn().mockName('load'),
+      },
+    };
+
+    const store = new Vuex.Store({
+      modules: {
+        repositories: repositoryModule,
+      },
+    });
+
+    const wrapper = shallowMount(RepositoryList, { localVue, store });
+
+    expect(wrapper.findAll('[data-testid="repository"]').at(0).text()).toBe('repo 1');
+    expect(wrapper.findAll('[data-testid="repository"]').at(1).text()).toBe('repo 2');
+  });
+});
