4459506d69b7983ef422d07f9ed71d9305fba476
Miguel Stevens <miguel@notflip.be>
2021-07-13 11:30:08 +0200

    Added code coverage to jest config

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
-};
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,vue}',
+    '!src/main.js',
+    '!src/store/index.js',
+    '!src/router/index.js',
+    '!src/views/**/*.{js,vue}'
+  ]
+}
