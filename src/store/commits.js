5ae9ff65f74d9f30cd270c6f4c65fc9d1c6d5421
Miguel Stevens <miguel@notflip.be>
2021-07-13 13:27:59 +0200

    Reset initialData when changing repository to prevent old data from being visible

--- src/store/commits.js
+++ src/store/commits.js
@@ -1,16 +1,22 @@
+const initialData = []
+
 export default (api) => ({
   namespaced: true,
   state: {
-    data: []
+    data: initialData
   },
   actions: {
     load: ({ commit }, repositoryId) => {
+      commit('resetState')
       api.loadCommits(repositoryId).then((data) => {
         commit('storeCommits', data)
       })
     }
   },
   mutations: {
+    resetState (state) {
+      state.data = initialData
+    },
     storeCommits (state, data) {
       state.data = data
     }
